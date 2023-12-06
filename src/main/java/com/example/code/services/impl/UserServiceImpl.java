56b13a50b468eb9666cb909b6ab6c02949ed164a
Stanislaw Shukan <shukans588@gmail.com>
2023-12-06 14:20:09 +0300

    Add profiles

--- src/main/java/com/example/code/services/impl/UserServiceImpl.java
+++ src/main/java/com/example/code/services/impl/UserServiceImpl.java
@@ -9,12 +9,14 @@ import com.example.code.mappers.UserMapper;
 import com.example.code.repositories.UserRepository;
 import com.example.code.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Profile("local")
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
