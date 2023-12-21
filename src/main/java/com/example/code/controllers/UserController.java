826d76b30e72a68b189589f1f70ebd504463dde0
Stanislaw Shukan <shukans588@gmail.com>
2023-12-21 13:54:00 +0300

    Rename methods in controller

--- src/main/java/com/example/code/controllers/UserController.java
+++ src/main/java/com/example/code/controllers/UserController.java
@@ -10,12 +10,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
@@ -43,7 +41,7 @@ public class UserController {
     public ResponseEntity<HttpStatus> update(@PathVariable("id") Long id,
                                              @RequestBody @Valid UserDto userDTO) {
         userService.update(userDTO, id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
